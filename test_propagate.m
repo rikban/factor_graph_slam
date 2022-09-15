272ee96527fa6a7d1316c550d8814e252c76f95c
Rik Banerjee <rik.banerjee1@gmail.com>
2022-09-15 13:36:32 -0600

    Added state propagation integration test

--- test_propagate.m
+++ test_propagate.m
@@ -1,13 +1,15 @@
 %% Unit test
 delta_t = 0.1;
 x_prev = [0;0;0];
-control_inputs = [2;1];
+control_inputs = [5;1];
 P_prev = eye(3);
 params = [1];  % Only parameter is diameter for now
-
+f= figure;
 for i = 0:10
  [x, P] =  propagate(delta_t, x_prev, P_prev, control_inputs, params);
  % Plot here
+ plot_trajectory_iterative(f, x, P)
+ axis equal
  x_prev = x;
  P_prev = P;
 end
