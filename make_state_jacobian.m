32d57c1e62dbd6aee59cf962ee1fe6074ccf497d
Mark Higger <markbhigger@gmail.com>
2022-09-16 10:33:28 -0600

    Fixed 0 omega case for jacobian

--- make_state_jacobian.m
+++ make_state_jacobian.m
@@ -9,8 +9,8 @@ function J = make_state_jacobian(v_c, w, theta, delta_t)
 %       J - [3x3] Jacobian matrix of the state transition
     if abs(w) < 1e-5
         J = [1, 0, 0;
-             0, 1, 0;
-             v_c*delta_t, 0, 1];
+             (v_c*delta_t)*cos(theta), 1, 0;
+             (v_c*delta_t)*sin(theta), 0, 1];
     else
         J = [1, 0, 0;
             (v_c/w)*cos(w*delta_t + theta), 1, 0;
